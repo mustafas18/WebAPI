c43f06e1ee3947e2ed2dd9829183deb684e3ccef
Mustafa Bazghandi <mostafabazghandi2001@gmail.com>
2022-01-03 11:46:29 +0330

    Fix routing

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : BaseController
     {
         private static readonly string[] Summaries = new[]
@@ -20,7 +20,7 @@ namespace WebAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast

5092d0418968e73565a35cf3916536ab27a9316f
Mustafa Bazghandi <mostafabazghandi2001@gmail.com>
2022-01-05 11:43:02 +0330

    Add LoginService_Return_InvalidAuthorization

--- UnitTest/Tests/LoginServiceTest.cs
+++ UnitTest/Tests/LoginServiceTest.cs
@@ -16,7 +16,40 @@ namespace UnitTest
     public class LoginServiceTest
     {
 
+        [Fact]
+        public async void LoginService_Return_InvalidAuthorization()
+        {
+            Mock<FakeUserManager> userManagerMock= new Mock<FakeUserManager>();
+            Mock<IJwtHandler> jwtHandlerMock= new Mock<IJwtHandler>();
 
+            UserDTO userDTO = new UserDTO
+            {
+                UserName = "ali",
+                Password = "P@ssw0rd123456"
+            };
+
+            User user = new User { UserName = userDTO.UserName, PasswordHash = "secret" };
+
+            var claims = new List<Claim>();
+            JwtSecurityToken tokenOptions = new JwtSecurityToken();
+            var key = Encoding.UTF8.GetBytes("secretSecuretyKey");
+            var secret = new SymmetricSecurityKey(key);
+            SigningCredentials signingCredentials = new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+
+         
+            userManagerMock.Setup(p => p.CheckPasswordAsync(user, userDTO.Password)).ReturnsAsync(true);
+
+            jwtHandlerMock.Setup(p => p.GetClaims(user)).Returns(new List<Claim> { new Claim(ClaimTypes.Name, user.UserName) });
+            jwtHandlerMock.Setup(x => x.GenerateTokenOptions(signingCredentials, claims)).Returns(tokenOptions);
+           
+
+            LoginService loginService = new LoginService(userManagerMock.Object, jwtHandlerMock.Object);
+            LoginResponceDTO expected =await loginService.GetAuthorizationAsync(userDTO);
+
+            //Assert
+            Assert.Equal(expected.ErrorMessage, "Invalid Authentication");
+
+        }
         [Fact]
         public async void LoginService_Should_Return_jwtToken()
         {
@@ -50,8 +83,8 @@ namespace UnitTest
                 jwtHandlerMock.Object);
             var expected = await loginService.GetAuthorizationAsync(userDTO);
 
-          
-             var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+         
+               var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
 
 
             Assert.Equal(expected.Token, "Bearer " + token);
